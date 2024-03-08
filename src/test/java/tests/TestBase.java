d62327b41f8b1aaa335d2f7b63c0a51356e284b1
alisitsky <alisitsky@gmail.com>
2024-03-08 13:57:10 +0100

    allure attachments added

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,13 +1,35 @@
 package tests;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
 import io.restassured.RestAssured;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+
+import static utils.AttachUtils.*;
 
 public class TestBase {
     @BeforeAll
     static void beforeAll() {
-        RestAssured.baseURI= "https://demoqa.com";
+        RestAssured.baseURI = "https://demoqa.com";
         Configuration.baseUrl = "https://demoqa.com";
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+    }
+
+    @AfterEach
+    void attachments() {
+        attachScreenshotAs("Last Step Screenshot");
+        attachPageSource();
+        attachBrowserConsoleLogs();
+        attachVideo();
+    }
+
+    @AfterEach
+    void afterEach() {
+        Selenide.clearBrowserCookies();
+        Selenide.clearBrowserLocalStorage();
+        Selenide.closeWebDriver();
     }
 }
